280a3d00fe71e404473d92ec7645edd1fcc4596a
kamwoo <rkadndud06@naver.com>
2021-06-13 15:23:34 +0900

    Initial Commit

---
+++ Realm_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Realm_Practice
+//
+//  Created by wooyeong kam on 2021/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
